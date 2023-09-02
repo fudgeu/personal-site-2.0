95c7a26a181adc0f69defc494e552595783b82ff
Fudgeu <patrickzambonie@gmail.com>
2023-09-01 23:40:15 -0400

    Initial Upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Gruppo, Roboto_Condensed, Rubik_Mono_One } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Rubik_Mono_One({
+	subsets: ['latin'],
+	weight: '400'
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,92 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className={styles.titleTextContainer}>
+				<span className={styles.subTitleText}>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+				</span>
+				<div className={styles.mainTitleText}>
+					<h1>FUDGEU</h1>
+				</div>
+				<span className={styles.subTitleText}>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+					<h1>FUDGEU</h1>
+				</span>
+			</div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+			<div className={styles.contentContainer}>
+				<div className={styles.webGLContainer}>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+				</div>
+				<div className={styles.textContentContainer}>
+					<h2>THE FUTURE IS NOW.</h2>
+					<div className={styles.descriptionAndNav}>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi volutpat viverra dui, sed lobortis augue elementum nec.</p>	
+						<nav>
+							<ul className={styles.navList}>
+								<li>&gt; ABOUT ME</li>
+								<li>&gt; PROJECTS</li>
+								<li>&gt; EDUCATION</li>
+								<li>&gt; CONTACT</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+			<div className={styles.cornerDecoration}>
+				<div className={styles.cornerDecorationLine}>
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+				</div>
+				
+				<div className={styles.cornerDecorationLineRev}>
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+				</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+				<div className={styles.cornerDecorationLine}>
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+					<div className={styles.cornerDecoRect} />
+					<div className={styles.cornerDecoCircle} />
+				</div>
+			</div>
+			
     </main>
   )
 }
