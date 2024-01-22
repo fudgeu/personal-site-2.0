a45a900da7f76e3b787d279b48c29926073ae9fe
Fudgeu <patrickzambonie@gmail.com>
2024-01-22 01:56:27 -0500

    Further work on contact page

--- app/(page)/contact/page.tsx
+++ app/(page)/contact/page.tsx
@@ -1,26 +1,130 @@
+'use client';
+
 import styles from './styles.module.css';
+import {
+  CSSProperties, ReactNode, useCallback, useEffect, useMemo, useRef, useState,
+} from 'react';
 import Marquee from 'react-fast-marquee';
 
+const backgroundIcons = [
+  '/contact/email.svg',
+  '/contact/discord-logo.svg',
+];
+
+type Bounds = {
+  x: number, y: number, eX: number, eY: number,
+};
+
 export default function Contact() {
+  const [backgroundDecorations, setBackgroundDecorations] = useState<ReactNode[]>([]);
+
+  const rootRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const allBounds = useRef<Bounds[]>([]);
+
+  const iconSize = useMemo(() => {
+    return Number(getComputedStyle(document.documentElement).fontSize.slice(0, -2)) * 3;
+  }, []);
+
+  const generateDecoration = useCallback(() => {
+    if (!rootRef.current || !allBounds.current) return;
+
+    // Generate size
+    const decW = 100 + Math.floor(Math.random() * 50);
+    const decH = iconSize * 1.25;
+
+    // Generate position
+    let generationSuccess = false;
+    let decXPos;
+    let decYPos;
+    let decBounds: Bounds = { x: 0, y: 0, eX: 0, eY: 0 };
+    for (let i = 0; i < 250; i++) { // failsafe in case random location can't be generated
+      const boundingRect = rootRef.current.getBoundingClientRect();
+      decXPos = Math.floor(Math.random() * (boundingRect.width - iconSize));
+      decYPos = Math.floor(Math.random() * (boundingRect.height - iconSize));
+      decBounds = {
+        x: boundingRect.x + decXPos,
+        y: boundingRect.y + decYPos,
+        eX: boundingRect.x + decXPos + decW,
+        eY: boundingRect.y + decYPos + decH,
+      };
+      const invalidLocation = allBounds.current.find((bounds) => {
+        return !(
+          decBounds.x > bounds.eX
+          || decBounds.eX < bounds.x
+          || decBounds.y > bounds.eY
+          || decBounds.eY < bounds.y
+        );
+      });
+      if (invalidLocation) continue;
+      else {
+        generationSuccess = true;
+        break;
+      }
+    }
+
+    if (!generationSuccess) console.warn('Generation failed, failsafe activated');
+
+    allBounds.current.push(decBounds);
+
+    const resultingStyle: CSSProperties = {
+      width: `${decW}px`,
+      height: `${decH}px`,
+      position: 'absolute',
+      top: `${decYPos}px`,
+      left: `${decXPos}px`,
+    };
+
+    // Randomly pick an icon
+    const icon = backgroundIcons[Math.floor(Math.random() * backgroundIcons.length)];
+    setBackgroundDecorations((prev) => {
+      return [...prev, (
+        <div className={styles.floatingBox} style={resultingStyle}>
+          <Marquee className={styles.marquee} speed={20} autoFill direction="right"><img src={icon} alt="" /></Marquee>
+        </div>
+      )];
+    });
+  }, [iconSize]);
+
+  // Get content bounds
+  useEffect(() => {
+    setTimeout(() => {
+      if (!contentRef.current) return;
+      const boundingBox = contentRef.current.getBoundingClientRect();
+      const contentBounds = {
+        x: boundingBox.x,
+        y: boundingBox.y,
+        eX: boundingBox.x + boundingBox.width,
+        eY: boundingBox.y + boundingBox.height,
+      };
+      allBounds.current.push(contentBounds);
+
+      for (let i = 0; i < 100; i++) {
+        setTimeout(() => generateDecoration(), (i + 1) * 20);
+      }
+    }, 100);
+  }, []);
+
   return (
-    <div>
-      <div className={styles.bkgDecoration}>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-        <Marquee autoFill><img className={styles.paperAirplane} src="/paper-plane.svg" alt="" /></Marquee>
-      </div>
+    <div className={styles.container} ref={rootRef}>
+      {backgroundDecorations.map((backgroundDecoration) => backgroundDecoration)}
 
       <div className={styles.contentContainer}>
-        <div className={styles.content}>
-
+        <div className={styles.content} ref={contentRef}>
+          <h1>CONTACT</h1>
+          <p>
+            IF YOU'D LIKE TO REACH OUT TO ME, WHETHER IT'S ABOUT A SPECIFIC REQUEST OR JUST TO TALK,
+            YOU CAN REACH ME AT:
+          </p>
+          <div className={styles.contact}>
+            <img src="/contact/discord-logo.svg" alt="Discord" />
+            FUDGEU
+          </div>
+          <div className={styles.contact}>
+            <img src="/contact/email.svg" alt="Email" />
+            PATRICKKOSS@OUTLOOK.COM
+          </div>
         </div>
       </div>
     </div>
--- eslint.config.js
+++ eslint.config.js
@@ -4,17 +4,29 @@ import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
 import reactJsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
 import reactHooks from 'eslint-plugin-react-hooks/cjs/eslint-plugin-react-hooks.development.js';
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
+  /* Browser environment */
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  /* Default flat configs */
   {
     ...reactRecommended,
     ...reactJsxRuntime,
     ...js.configs.recommended,
   },
+  /* React hooks config */
   {
     plugins: { 'react-hooks': reactHooks }, // react hooks' config are not yet flat config ready
     rules: reactHooks.configs.recommended.rules,
   },
+  /* Stylistic flat config factory */
   {
     ...stylistic.configs.customize({
       semi: true,
@@ -26,52 +38,8 @@ export default [
       parser: parserTs,
     },
     files: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
-    /* 'rules': {
-      'stylistic/semi': 'error',
-      'stylistic/array-bracket-spacing': [
-        'error',
-        'never',
-        { 'singleValue': false, 'objectsInArrays': false, 'arraysInArrays': false },
-      ],
-      'stylistic/arrow-spacing': 'error',
-      'stylistic/block-spacing': 'error',
-      'stylistic/comma-spacing': 'error',
-      'stylistic/computed-property-spacing': ['error', 'never'],
-      'stylistic/function-call-spacing': ['error', 'never'],
-      'stylistic/jsx-curly-spacing': [2, 'never'],
-      'stylistic/jsx-equals-spacing': [2, 'never'],
-      'stylistic/jsx-tag-spacing': 'error',
-      'stylistic/key-spacing': 'error',
-      'stylistic/keyword-spacing': 'error',
-      'stylistic/no-mixed-spaces-and-tabs': 'error',
-      'stylistic/no-multi-spaces': 'error',
-      'stylistic/no-trailing-spaces': 'error',
-      'stylistic/no-whitespace-before-property': 'error',
-      'stylistic/rest-spread-spacing': ['error', 'never'],
-      'stylistic/semi-spacing': 'error',
-      'stylistic/space-before-blocks': 'error',
-      'stylistic/space-before-function-paren': [
-        'error',
-        { 'anonymous': 'never', 'named': 'never', 'asyncArrow': 'always' }
-      ],
-      'stylistic/space-in-parens': ['error', 'never'],
-      'stylistic/space-infix-ops': 'error',
-      'stylistic/space-unary-ops': 'error',
-      'stylistic/spaced-comment': ['error', 'always'],
-      'stylistic/switch-colon-spacing': 'error',
-      'stylistic/template-curly-spacing': ['error', 'never'],
-      'stylistic/type-annotation-spacing': 'error',
-      'stylistic/type-generic-spacing': 'error',
-      'stylistic/eol-last': ['error', 'always'],
-      'stylistic/function-paren-newline': ['error', 'multiline'],
-      'stylistic/implicit-arrow-linebreak': ['error', 'beside'],
-      'stylistic/jsx-curly-newline': ['error', 'never'],
-      'stylistic/quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
-      'stylistic/indent': ['error', 2],
-      'stylistic/object-curly-spacing': ['error', 'always'],
-      'no-unused-vars': 'warn',
-    } */
   },
+  /* Stylistic overrides */
   {
     plugins: { stylistic },
     languageOptions: {
